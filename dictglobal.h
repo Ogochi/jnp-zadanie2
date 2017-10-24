387a548df9e88ad014400596b4598b6520104a63
Michał Ołtarzewski <michalo@radcode.co>
2017-10-24 12:27:49 +0200

    Added dictglobal.h api

---
+++ dictglobal.h
@@ -0,0 +1,12 @@
+unsigned long dict_global();
+/*
+    Zwraca identyfikator globalnego słownika, którego nie można usunąć.
+*/
+
+const int MAX_GLOBAL_DICT_SIZE = 42;
+/*
+    Stała określa maksymalny rozmiar globalnego słownika.
+    Do globalnego słownika można wstawiać kolejne klucze z wartościami tylko,
+    gdy jego rozmiar po wstawieniu nie będzie przekraczał maksymalnego
+    rozmiaru.
+*/
