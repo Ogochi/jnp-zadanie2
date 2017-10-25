048dd6601b511f3de9a5989ce86781a6e4e44110
Marian Dabrowski <marian.dabrowski95@gmail.com>
2017-10-25 21:44:33 +0200

    defined copy func

--- dict.cc
+++ dict.cc
@@ -42,7 +42,25 @@ const char* dict_find(unsigned long id, const char* key);
 
 void dict_clear(unsigned long id);
 
-void dict_copy(unsigned long src_id, unsigned long dst_id);
+void dict_copy(unsigned long src_id, unsigned long dst_id) {
+	DICT dict_to_copy = dicts[src_id];
+	string key_copy, value_copy;
+	for (auto key_value_pair : dict_to_copy) {
+		key_copy =  key_value_pair.first.copy();
+		value_copy = key_value_pair.second.copy();
+		dict_insert(dict_to_copy, key_copy, value_copy);
+	}
+	/*
+	// nie wiem czy kopiujemy kluczy czy nie
+	// czy mamy dac nacisk na leniwy kod jak ten ponizej czy bardziej na taki 
+	// zrozumialy jak u gory
+	
+	// alternatywnie
+	for (auto key_value_pair : dict_to_copy) {
+		dict_insert(dict_to_kopy, key_value_pair.first, key_value_pair.second)
+	}
+	*/
+}
 
 int main() {
 
