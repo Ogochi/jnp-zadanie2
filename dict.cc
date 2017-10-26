654ad6fbbd1fe30f2572b6bcbed77cab23b8efa7
Blazej Mroz <b.mroz@students.mimuw.edu.pl>
2017-10-26 10:36:58 +0200

    Fixed compilation errors for B. functions

--- dict.cc
+++ dict.cc
@@ -1,6 +1,9 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include "dict.h"
+#include "dictglobal.h"
 
 #ifdef NDEBUG
   const bool debug = false;
@@ -8,8 +11,10 @@
   const bool debug = true;
 #endif
 
+using std::size_t;
 using std::string;
 using std::unordered_map;
+using std::make_pair;
 using DICT = unordered_map<string, string>;
 
 static unordered_map<unsigned long, DICT > dicts ( {{0, DICT ()}} );
@@ -38,10 +43,10 @@ size_t dict_size(unsigned long id) {
 void dict_insert(unsigned long id, const char* key, const char* value) {
   if (key == NULL ||  value == NULL)
     return;
-  auto dict = dicts[id];
-  if (dict == dicts.end())
+  auto dictIter = dicts.find(id);
+  if (dictIter == dicts.end())
     return;
-  dict.insert(make_pair<string,string>(key, value));
+  dictIter->second.insert(std::make_pair<string,string>(key, value));
 }
 
 void dict_remove(unsigned long id, const char* key) {
@@ -50,34 +55,36 @@ void dict_remove(unsigned long id, const char* key) {
     klucz oraz wartość związaną z tym kluczem, a w przeciwnym przypadku nic nie
     robi.
 */
-  auto dict = dicts[id];
-  if (dict != dicts.end() && dict[key] != dict.end()) {
-    dict.erase(key);
+  auto dictIter = dicts.find(id);
+  if (dictIter != dicts.end() && 
+      dictIter->second.find(key) != dictIter->second.end()) {
+    dictIter->second.erase(key);
   }
 }
 
 const char* dict_find(unsigned long id, const char* key) {
-  auto dict = dicts[id];
+  auto dictIter = dicts.find(id);
   string s_key(key);
   
   // Nie istnieje słownik lub klucz nie istnieje w podanym słowniku.
-  if (dict == dicts.end() || dict[s_key] == dict.end()) {
-    auto global = dicts[dict_global()];
-    auto found = global[s_key];
-    if (found == global.end()) 
+  if (dictIter == dicts.end() || 
+      dictIter->second.find(key) == dictIter->second.end()) {
+    auto globalIter = dicts.find(dict_global());
+    auto foundIter = globalIter->second.find(s_key);
+    if (foundIter == globalIter->second.end()) 
       return NULL;
     else 
-      return found.c_str();
+      return foundIter->second.c_str();
   } 
   else {
-    return dict[s_key].c_str();
+    return dictIter->second.find(key)->second.c_str();
   }
 }
 
 void dict_clear(unsigned long id) {
-  auto dict = dicts[id];
-  if (dict != dicts.end())
-    dict.clear();
+  auto dictIter = dicts.find(id);
+  if (dictIter != dicts.end())
+    dictIter->second.clear();
 }
 
 void dict_copy(unsigned long src_id, unsigned long dst_id) {
